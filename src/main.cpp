c5d5fd705d7722413e76185643fc2e74b13f2ee5
Dimitris Siountris <dsiountris@emphasisdw.com>
2026-06-11 20:12:55 +0300

    Create Basic Logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,42 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 int main()
 {
+    std::string textInput;
+
     while (true) {
-        std::string name;
+        
+        std::string innerText;
+        std::cout << "Send me text to save it or send me DLT in order to delete and startover...\n Enter the text...  ";
+        std::getline(std::cin, innerText);
+        textInput += innerText + '\n';
 
-        std::cout << "Enter your name: ";
-        std::getline(std::cin, name);
+        if (innerText == "DLT") {
+            std::ofstream file("test.csv");
 
-        std::cout << "Hello, " << name << "!" << std::endl;
+            file.close();
+            textInput.clear();
+
+            std::cout << "File cleared.\n";
+
+        } 
+        else {
+             std::ofstream file("test.csv", std::ios::app);
+
+            if (!file.is_open())
+            {
+                std::cerr << "Failed to open file\n";
+                return 1;
+            }
+
+            file << innerText << '\n';
+
+            file.close();
+             std::cout << "Text Saved successfully, " << textInput << "!" << std::endl;
+        }
+       
     }
 
     return 0;
